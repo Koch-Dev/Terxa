b97ff3e788fed8d99dfcc2b7c738f6eb93b9ea5d
Chiranjib <93111779+Koch-Dev@users.noreply.github.com>
2024-04-02 21:56:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 API_ID = 21309577  # api id
-API_HASH = "df2554b54a9eb9e572979b5db2d0cc79""  # api hash
+API_HASH = "df2554b54a9eb9e572979b5db2d0cc79"  # api hash
 
-BOT_TOKEN = "df2554b54a9eb9e572979b5db2d0cc79""  # bot token
+BOT_TOKEN = "6826558225:AAEctOA7vCX-skVaUg6NegxnMhkZUa0pUMo"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+HOST = "redis-10766.c322.us-east-1-2.ec2.cloud.redislabs.com"  # redis host uri
+PORT = 10766  # redis port
+PASSWORD = "SpqESSKT6yH2k6USgDejhWfiOB72ItBy""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1001620341107 # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+ADMINS = [5218610039]
